29f0b8d1aa5f0e84973d72ba69e8bd1ef0b2f259
Suraj Nair <surajnair9698@gmail.com>
2025-01-21 00:47:14 +0530

    Refactoring the previous code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,6 +9,12 @@ public_users.post("/register", (req, res) => {
   return res.status(300).json({ message: "Yet to be implemented" });
 });
 
+const getBooks = () => {
+  return new Promise((resolve, reject) => {
+    resolve(books);
+  });
+};
+
 // Get the book list available in the shop
 public_users.get("/", async function (req, res) {
   try {
