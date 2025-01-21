eb2e58c0fa37009444473fa21218b3a24b650226
Suraj Nair <surajnair9698@gmail.com>
2025-01-21 16:04:23 +0530

    Task 4 & 5 completed

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -8,7 +8,11 @@ let books = {
   3: { author: "Dante Alighieri", title: "The Divine Comedy", reviews: {} },
   4: { author: "Unknown", title: "The Epic Of Gilgamesh", reviews: {} },
   5: { author: "Unknown", title: "The Book Of Job", reviews: {} },
-  6: { author: "Unknown", title: "One Thousand and One Nights", reviews: {} },
+  6: {
+    author: "Unknown",
+    title: "One Thousand and One Nights",
+    reviews: { Rinzler: "Great Read" },
+  },
   7: { author: "Unknown", title: "Nj\u00e1l's Saga", reviews: {} },
   8: { author: "Jane Austen", title: "Pride and Prejudice", reviews: {} },
   9: {
